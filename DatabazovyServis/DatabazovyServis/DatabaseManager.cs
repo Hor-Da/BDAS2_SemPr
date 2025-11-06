0b98dc3f32a7af907accea3051488bce19ea2135
matejlanfi <136738970+matejlanfi@users.noreply.github.com>
2025-11-06 16:26:22 +0100

    Commit 1

---
+++ DatabazovyServis/DatabazovyServis/DatabaseManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DatabazovyServis
+{
+	internal class DatabaseManager
+	{
+		private string username;
+		private string password;
+		
+		public void SetCredentials(string user, string pwd)
+		{
+			username = user;
+			password = pwd;
+		}
+
+		public OracleConnection GetConnection()
+		{
+			string connectionString = $"User Id={username};Password={password};" +
+			"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=fei-sql3.upceucebny.cz)(PORT=1521)))(CONNECT_DATA=(SID=BDAS)))";
+			return new OracleConnection(connectionString);
+		}
+	}
+}
