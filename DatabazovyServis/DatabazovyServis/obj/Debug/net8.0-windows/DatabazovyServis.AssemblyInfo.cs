8f1c50a0998076c0b51f4cd79568f925bd16c4e3
matejlanfi <136738970+matejlanfi@users.noreply.github.com>
2025-11-06 16:47:41 +0100

    Commit 1.1

--- DatabazovyServis/DatabazovyServis/obj/Debug/net8.0-windows/DatabazovyServis.AssemblyInfo.cs
+++ DatabazovyServis/DatabazovyServis/obj/Debug/net8.0-windows/DatabazovyServis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabazovyServis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b98dc3f32a7af907accea3051488bce19ea2135")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabazovyServis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabazovyServis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
